82c4c5409992ce63d890598fddfafab4551a2778
ljw <84855512@qq.com>
2024-06-14 16:27:29 +0800

    add safego

--- utils/tools.go
+++ utils/tools.go
@@ -4,6 +4,8 @@ import (
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"runtime/debug"
 )
 
 func Md5(str string) string {
@@ -13,13 +15,49 @@ func Md5(str string) string {
 
 func CopyStructByJson(src, dst interface{}) {
 	str, _ := json.Marshal(src)
-	json.Unmarshal(str, dst)
+	err := json.Unmarshal(str, dst)
+	if err != nil {
+		return
+	}
 }
 
-//CopyStructToMap 结构体转map
+// CopyStructToMap 结构体转map
 func CopyStructToMap(src interface{}) map[string]interface{} {
 	var res = map[string]interface{}{}
 	str, _ := json.Marshal(src)
-	json.Unmarshal(str, &res)
+	err := json.Unmarshal(str, &res)
+	if err != nil {
+		return nil
+	}
 	return res
+}
+
+// SafeGo is a common function to recover panic for goroutines
+func SafeGo(f interface{}, params ...interface{}) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("Recovered in SafeGo: %v\n", r)
+				debug.PrintStack()
+			}
+		}()
+
+		// Convert f to a reflect.Value
+		funcValue := reflect.ValueOf(f)
+
+		// Check if the f is a function
+		if funcValue.Kind() != reflect.Func {
+			fmt.Println("SafeGo: value is not a function")
+			return
+		}
+
+		// Convert params to reflect.Value
+		paramsValue := make([]reflect.Value, len(params))
+		for i, param := range params {
+			paramsValue[i] = reflect.ValueOf(param)
+		}
+
+		// Call the function f with params
+		funcValue.Call(paramsValue)
+	}()
 }
